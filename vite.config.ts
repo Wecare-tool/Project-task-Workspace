0902244c1176b467ffa4dbe44c038ba6d31b433a
Hoang <hieu.le@wecare-i.com>
2025-12-09 09:26:25 +0700

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/Project-task-Workspace/',
     plugins: [react()],
     resolve: {
         alias: {
